7648e1b172772b6ba5429d4a24c97f6114c5e36d
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-09-29 23:46:42 -0400

    Some basic UI organization

---
+++ src/components/HomeScreen.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  user: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+    <header>Welcome to the Useful List App</header>
+    <h1 class="green">Hello {{ user }}</h1>
+</template>
+
+<style scoped>
+
+</style>
