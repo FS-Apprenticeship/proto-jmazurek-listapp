b7b5adbb17586bb19fc740337bc2e206599d6401
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-01 14:53:40 -0400

    Get the list to pull from the DB

---
+++ src/components/List/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  email: {
+    type: String,
+    required: true,
+  },
+
+})
+</script>
+
+<template>
+    <header><h1>Welcome to the Useful List App {{ email }}</h1></header>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -1,18 +1,41 @@
 <script setup>
-import { ref } from 'vue'
-import Item from './Item.vue';
+// {
+//     name: "Idea!",
+//     // message: String, //Use to add more detail, but not right now
+//     crossedOff: false,
+//     trashed: false,
+// }
 
-const list = ref([
-    {
-        name: "Idea!",
-        // message: String, //Use to add more detail, but not right now
-        crossedOff: false,
-        trashed: false,
+import { ref, watchEffect } from 'vue'
+import Item from './Item.vue';
+import { addListItem, getEmail, getListItems } from '@/database/database-control';
+import Header from './Header.vue';
+
+const email = getEmail();
+
+const list = ref([]);
+
+function refresh() {
+    getListItems().then((items) => list.value = items);
+}
+
+function addOnAnotherItemTemporary() {
+    const something = {
+    name: "Something :|",
+    crossedOff: false,
+    trashed: false,
     }
-]);
+
+    addListItem(something);
+    refresh();
+}
+
+refresh();
 </script>
 
 <template>
+    <Header :email="email" />
+    <button @click="addOnAnotherItemTemporary">Add something!</button>
   <ul>
     <Item v-for="item in list" :key="item.id" :item="item" />
   </ul>
