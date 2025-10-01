4a677fb1cf96dbb0979203a2b09739611fd35c06
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-09-30 23:15:51 -0400

    Add more to Supabase control

---
+++ src/database/supabase-control.js
@@ -0,0 +1,44 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+
+let user, session;
+
+export async function signUpNewUser(email, password) {
+  const { data, error } = await supabase.auth.signUp({email, password});
+
+  if (error !== null) return false;
+
+  user = data.user;
+  session = data.session;
+
+  return true;
+}
+
+export async function signInWithEmail(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({email, password});
+
+  if (error !== null) return false;
+
+  user = data.user;
+  session = data.session;
+
+  return true;
+}
+
+export async function getListItems() {
+
+}
+
+export async function addListItem(item) {
+
+}
+
+export async function updateListItem(item) {
+
+}
+
+export async function deleteListItem(item) {
+  
+}
