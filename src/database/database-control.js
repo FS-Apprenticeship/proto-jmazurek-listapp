54edccebf048bab2ff4bfd6c92e19059737a1a47
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-01 09:24:11 -0400

    Create a basic fake database

---
+++ src/database/database-control.js
@@ -0,0 +1,3 @@
+import databaseControls from './faux-database-control';
+
+export default databaseControls;
---
+++ src/database/faux-database-control.js
@@ -0,0 +1,63 @@
+const genericDatabase = [];
+const users = [];
+
+let user, session;
+
+export async function signUpNewUser(email, password) {
+  users.sort((a, b) => a.id - b.id);
+
+  let previousID = -1;
+
+  const newUser = {
+    id: users.findIndex(user => {
+      return user.id === ++previousID;
+    }),
+    email,
+    password //Obviously evil!
+  }
+
+  users.push(newUser);
+
+  user = newUser;
+
+  return true;
+}
+
+export async function signInWithEmail(email, password) {
+  let userToLogin = users.find(user => user.email === email);
+
+  if (userToLogin === undefined) return false;
+  if (userToLogin.password !== password) return false;
+
+  user = userToLogin;
+
+  return true;
+}
+
+export async function getListItems() {
+  return genericDatabase.filter(item => item.userID === user.id);
+}
+
+export async function addListItem(item) {
+  genericDatabase.push({...item, userID: user.id});
+}
+
+export async function updateListItem(itemUpdate) {
+  const databaseIndex = genericDatabase.findIndex(item => item.id === itemUpdate.id && item.userID === user.id);
+
+  if (databaseIndex === -1) return false;
+
+  genericDatabase[databaseIndex] = itemUpdate;
+
+  return getListItems();
+}
+
+export async function deleteListItem(itemToDelete) {
+  const databaseIndex = genericDatabase.findIndex(item => item.id === itemToDelete.id && item.userID === user.id);
+
+  if (databaseIndex === -1) return false;
+
+  genericDatabase.splice(databaseIndex, 1);
+
+  return getListItems();
+}
