f452b8982349e02c433a33bd67f2e2146eda8db4
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-01 15:13:55 -0400

    Create the item generator and editor

---
+++ src/components/List/EditableItem.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref } from 'vue';
+    const { item } = defineProps({
+        item: {
+            type: {
+                name: String,
+                message: String, //Use to add more detail, but not right now
+                crossedOff: Boolean,
+                trashed: Boolean,
+            },
+            required: true,
+        }
+    })
+
+    defineEmits(['update']);
+
+    const itemName = ref(item.name);
+
+</script>
+
+<template>
+    <li v-if="mode === 'View'" class="item">
+        <input v-model="itemName" />
+        <button class="cross-button green" @click="$update('update', item.name)">✔</button>
+    </li>
+</template>
+
+<style scoped>
+    .item {
+        width: 50%;
+        border: solid 1px black;
+        border-radius: 20px;
+        
+        display: flex;
+        flex-direction: row;
+    }
+
+    .cross-button {
+        width: 10%;
+        border: 0px;
+        border-top-right-radius: 20px;
+        border-bottom-right-radius: 20px;
+    }
+
+    input {
+        width: 75%;
+        padding-left: 5%;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+    }
+
+    .green {
+        background-color: green;
+        color: white;
+    }
+
+    .green:hover {
+        background-color: #159515;
+    }
+
+
+    .red {
+        background-color: #d00000;
+        color: white;
+    }
+
+    .red:hover {
+        background-color: #f50000;
+    }
+</style>
