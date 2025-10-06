3777ace9de0b40fd22a7655ec16be5b81bd44fd9
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-06 11:17:01 -0400

    Add New List displays

---
+++ src/components/Lists/EditableList.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { updateListName } from '@/database/multiple-list-control';
+import { ref } from 'vue';
+    const { list } = defineProps({
+        list: {
+            type: {
+                name: String,
+            },
+            required: true,
+        }
+    })
+
+    const emit = defineEmits(['complete']);
+
+    const listName = ref(list.name);
+
+    function update() {
+        const success = updateListName(listName, list.id)
+        if (success) emit('complete')
+    }
+
+</script>
+
+<template>
+    <li class="list">
+        <input v-model="listName" @keypress.enter="update"/>
+        <button class="cross-button green" @click="update">✔</button>
+    </li>
+</template>
+
+<style scoped>
+    .cross-button {
+        width: 10%;
+        border: 0px;
+        border-top-right-radius: 20px;
+        border-bottom-right-radius: 20px;
+    }
+
+    input {
+        font-size: larger;
+        width: 85%;
+        padding-left: 5%;
+        border: none;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+    }
+
+    .green {
+        background-color: green;
+        color: white;
+    }
+
+    .green:hover {
+        background-color: #159515;
+    }
+
+
+    .red {
+        background-color: #d00000;
+        color: white;
+    }
+
+    .red:hover {
+        background-color: #f50000;
+    }
+</style>
---
+++ src/components/Lists/SingleList.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { deleteThisList, restoreList, trashList } from '@/database/multiple-list-control';
+
+    defineProps({
+    list: {
+        type: {
+            name: String,
+            id: String,
+            trashed: Boolean
+        },
+        required: true,
+    },
+    })
+
+    defineEmits('edit');
+
+</script>
+
+<template>
+    <li class="list">
+        <h3>{{ item.name }}</h3>
+        <button v-if="!list.trashed" class="edit" @click="$emit('edit')">⚙</button>
+        <div class="delete">
+            <button v-if="!list.trashed" id="trash-button" class="red" @click="trashList(list.id)">🗑︎</button>
+            <div v-else>
+                <button id="delete-button" class="red" @click="deleteThisList(list.id)">Delete!</button>
+                <button id="restore-button" class="green" @click="restoreList(list.id)">↩︎ Restore</button>
+            </div>
+        </div>
+    </li>
+</template>
+
+<style scoped>
+    .cross-button {
+        width: 10%;
+        border: none;
+        border-right: 1px solid black;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+    }
+
+    h3 {
+        width: 70%;
+        padding-left: 5%;
+    }
+
+    .edit {
+        font-size: larger;
+        width: 5%;
+        justify-content: center;
+        border: none;
+        border-left: 1px solid black;
+    }
+
+    .delete {
+        margin-left: auto;
+        width: 10%;
+        border-bottom-right-radius: 20px;
+    }
+
+    .delete * {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    #trash-button {
+        border-top-right-radius: 20px;
+        border-bottom-right-radius: 20px;
+        border: 0px;
+    }
+
+    #delete-button {
+        border-top-right-radius: 20px;
+        border: 0px;
+    }
+
+    #restore-button {
+        border-bottom-right-radius: 20px;
+        border: 0px;
+    }
+
+    .green {
+        background-color: green;
+        color: white;
+    }
+
+    .green:hover {
+        background-color: #159515;
+    }
+
+
+    .red {
+        background-color: #d00000;
+        color: white;
+    }
+
+    .red:hover {
+        background-color: #f50000;
+    }
+</style>
